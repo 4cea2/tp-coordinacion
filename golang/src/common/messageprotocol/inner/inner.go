73b1f812e0cf239068cb3c85014d38d90b70e0a2
4cea2 <kcarbajal@fi.uba.ar>
2026-04-15 17:00:46 -0300

    feat: add clientID to inner protocol and message handler

--- golang/src/common/messageprotocol/inner/inner.go
+++ golang/src/common/messageprotocol/inner/inner.go
@@ -8,8 +8,13 @@ import (
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
 )
 
-func serializeJson(message []interface{}) ([]byte, error) {
-	return json.Marshal(message)
+type MessageClient struct {
+	ClientId int64         `json:"client_id"`
+	Data     []interface{} `json:"data"`
+}
+
+func serializeJson(messageClient MessageClient) ([]byte, error) {
+	return json.Marshal(messageClient)
 }
 
 func deserializeJson(message []byte) ([]interface{}, error) {
@@ -20,17 +25,19 @@ func deserializeJson(message []byte) ([]interface{}, error) {
 	return data, nil
 }
 
-func SerializeMessage(fruitRecords []fruititem.FruitItem) (*middleware.Message, error) {
-	data := []interface{}{}
+func SerializeMessage(fruitRecords []fruititem.FruitItem, clientID int64) (*middleware.Message, error) {
+	fruitData := []interface{}{}
 	for _, fruitRecord := range fruitRecords {
 		datum := []interface{}{
 			fruitRecord.Fruit,
 			fruitRecord.Amount,
 		}
-		data = append(data, datum)
+		fruitData = append(fruitData, datum)
 	}
 
-	body, err := serializeJson(data)
+	msgClient := MessageClient{clientID, fruitData}
+
+	body, err := serializeJson(msgClient)
 	if err != nil {
 		return nil, err
 	}
@@ -39,32 +46,33 @@ func SerializeMessage(fruitRecords []fruititem.FruitItem) (*middleware.Message, 
 	return &message, nil
 }
 
-func DeserializeMessage(message *middleware.Message) ([]fruititem.FruitItem, bool, error) {
-	data, err := deserializeJson([]byte((*message).Body))
+func DeserializeMessage(message *middleware.Message) ([]fruititem.FruitItem, int64, bool, error) {
+	var msgClient MessageClient
+	err := json.Unmarshal([]byte(message.Body), &msgClient)
 	if err != nil {
-		return nil, false, err
+		return nil, 0, false, err
 	}
 
 	fruitRecords := []fruititem.FruitItem{}
-	for _, datum := range data {
+	for _, datum := range msgClient.Data {
 		fruitPair, ok := datum.([]interface{})
 		if !ok {
-			return nil, false, errors.New("Datum is not an array")
+			return nil, 0, false, errors.New("Datum is not an array")
 		}
 
 		fruit, ok := fruitPair[0].(string)
 		if !ok {
-			return nil, false, errors.New("Datum is not a (fruit, amount) pair")
+			return nil, 0, false, errors.New("Datum is not a (fruit, amount) pair")
 		}
 
 		fruitAmount, ok := fruitPair[1].(float64)
 		if !ok {
-			return nil, false, errors.New("Datum is not a (fruit, amount) pair")
+			return nil, 0, false, errors.New("Datum is not a (fruit, amount) pair")
 		}
 
 		fruitRecord := fruititem.FruitItem{Fruit: fruit, Amount: uint32(fruitAmount)}
 		fruitRecords = append(fruitRecords, fruitRecord)
 	}
 
-	return fruitRecords, len(fruitRecords) == 0, nil
+	return fruitRecords, msgClient.ClientId, len(fruitRecords) == 0, nil
 }
--- golang/src/gateway/messagehandler/messagehandler.go
+++ golang/src/gateway/messagehandler/messagehandler.go
@@ -1,32 +1,40 @@
 package messagehandler
 
 import (
+	"errors"
+	"time"
+
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/fruititem"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/messageprotocol/inner"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
 )
 
 type MessageHandler struct {
+	clientID int64
 }
 
 func NewMessageHandler() MessageHandler {
-	return MessageHandler{}
+	id := time.Now().UnixNano()
+	return MessageHandler{clientID: id}
 }
 
 func (messageHandler *MessageHandler) SerializeDataMessage(fruitRecord fruititem.FruitItem) (*middleware.Message, error) {
 	data := []fruititem.FruitItem{fruitRecord}
-	return inner.SerializeMessage(data)
+	return inner.SerializeMessage(data, messageHandler.clientID)
 }
 
 func (messageHandler *MessageHandler) SerializeEOFMessage() (*middleware.Message, error) {
 	data := []fruititem.FruitItem{}
-	return inner.SerializeMessage(data)
+	return inner.SerializeMessage(data, messageHandler.clientID)
 }
 
 func (messageHandler *MessageHandler) DeserializeResultMessage(message *middleware.Message) ([]fruititem.FruitItem, error) {
-	fruitRecords, _, err := inner.DeserializeMessage(message)
+	fruitRecords, clientID, _, err := inner.DeserializeMessage(message)
 	if err != nil {
 		return nil, err
+	}
+	if clientID != messageHandler.clientID {
+		return nil, errors.New("Client ID mismatch")
 	}
 	return fruitRecords, nil
 }

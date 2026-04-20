766f53ba26753b4d0e84ab71547fd2912652e163
4cea2 <kcarbajal@fi.uba.ar>
2026-04-20 15:51:02 -0300

    feat: join calculates top final and send gateway

--- golang/src/join/join/join.go
+++ golang/src/join/join/join.go
@@ -2,7 +2,9 @@ package join
 
 import (
 	"log/slog"
+	"sort"
 
+	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/fruititem"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/messageprotocol/inner"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
 )
@@ -20,8 +22,10 @@ type JoinConfig struct {
 }
 
 type Join struct {
-	inputQueue  middleware.Middleware
-	outputQueue middleware.Middleware
+	inputQueue   middleware.Middleware
+	outputQueue  middleware.Middleware
+	clientFruits map[int64]map[string]fruititem.FruitItem
+	config       JoinConfig
 }
 
 func NewJoin(config JoinConfig) (*Join, error) {
@@ -38,7 +42,11 @@ func NewJoin(config JoinConfig) (*Join, error) {
 		return nil, err
 	}
 
-	return &Join{inputQueue: inputQueue, outputQueue: outputQueue}, nil
+	return &Join{inputQueue: inputQueue,
+		outputQueue:  outputQueue,
+		clientFruits: map[int64]map[string]fruititem.FruitItem{},
+		config:       config,
+	}, nil
 }
 
 func (join *Join) Run() {
@@ -49,10 +57,42 @@ func (join *Join) Run() {
 
 func (join *Join) handleMessage(msg middleware.Message, ack func(), nack func()) {
 	defer ack()
-	fruitRecords, clientID, _, _ := inner.DeserializeMessage(&msg)
+	fruitRecords, clientID, isEof, _ := inner.DeserializeMessage(&msg)
 
-	slog.Info("top fruits per client", "fruits", fruitRecords, "clientID", clientID)
-	if err := join.outputQueue.Send(msg); err != nil {
-		slog.Error("While sending top", "err", err)
+	if isEof {
+		slog.Info("Received End Of Records message", "clientID", clientID)
+		fruitsTop := join.buildFruitTop(join.clientFruits[clientID]) // y si el cliente no me manda nada, solo un eof?
+		slog.Info("top fruits per client", "fruits", fruitsTop, "clientID", clientID)
+		message, err := inner.SerializeMessage(fruitsTop, clientID)
+		if err != nil {
+			slog.Error("While serialize top", "err", err)
+		}
+		if err = join.outputQueue.Send(*message); err != nil {
+			slog.Error("While sending top", "err", err)
+		}
+	} else {
+		if _, ok := join.clientFruits[clientID]; !ok {
+			join.clientFruits[clientID] = map[string]fruititem.FruitItem{}
+		}
+
+		for _, fruitRecord := range fruitRecords {
+			if _, ok := join.clientFruits[clientID][fruitRecord.Fruit]; ok {
+				join.clientFruits[clientID][fruitRecord.Fruit] = join.clientFruits[clientID][fruitRecord.Fruit].Sum(fruitRecord)
+			} else {
+				join.clientFruits[clientID][fruitRecord.Fruit] = fruitRecord
+			}
+		}
 	}
+}
+
+func (join *Join) buildFruitTop(fruitsMap map[string]fruititem.FruitItem) []fruititem.FruitItem {
+	fruitItems := make([]fruititem.FruitItem, 0, len(fruitsMap))
+	for _, item := range fruitsMap {
+		fruitItems = append(fruitItems, item)
+	}
+	sort.SliceStable(fruitItems, func(i, j int) bool {
+		return fruitItems[j].Less(fruitItems[i])
+	})
+	finalTopSize := min(join.config.TopSize, len(fruitItems))
+	return fruitItems[:finalTopSize]
 }
